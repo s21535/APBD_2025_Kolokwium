085254b25eab3737d548d4185927ef53b7e994bc
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-06-08 09:32:55 +0200

    Dodano migrację oraz bazowy Controller

---
+++ Shows.API/Shows.API/Migrations/20250608073008_Initial.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Shows.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Concerts",
+                columns: table => new
+                {
+                    ConcertId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AvailableTickets = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concerts", x => x.ConcertId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SerialNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SeatNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketConcerts",
+                columns: table => new
+                {
+                    TicketConcertId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    TicketId = table.Column<int>(type: "int", nullable: false),
+                    ConcertId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketConcerts", x => x.TicketConcertId);
+                    table.ForeignKey(
+                        name: "FK_TicketConcerts_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketConcerts_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Purchased_Ticket",
+                columns: table => new
+                {
+                    TicketConcertId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchased_Ticket", x => new { x.TicketConcertId, x.CustomerId });
+                    table.ForeignKey(
+                        name: "FK_Purchased_Ticket_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Purchased_Ticket_TicketConcerts_TicketConcertId",
+                        column: x => x.TicketConcertId,
+                        principalTable: "TicketConcerts",
+                        principalColumn: "TicketConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Concerts",
+                columns: new[] { "ConcertId", "AvailableTickets", "Date", "Name" },
+                values: new object[,]
+                {
+                    { 1, 25000, new DateTime(2025, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Aerosmith" },
+                    { 2, 10000, new DateTime(2026, 7, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), "Primus" },
+                    { 3, 100000, new DateTime(2025, 7, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ozzy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "Mac", "McDonald", "555-123-1234" },
+                    { 2, "Sam", "Smith", null },
+                    { 3, "Jack", "Jones", "555-333-4444" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tickets",
+                columns: new[] { "TicketId", "SeatNumber", "SerialNumber" },
+                values: new object[,]
+                {
+                    { 1, 543, "111" },
+                    { 2, 210, "222" },
+                    { 3, 5001, "1540A" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TicketConcerts",
+                columns: new[] { "TicketConcertId", "ConcertId", "Price", "TicketId" },
+                values: new object[,]
+                {
+                    { 1, 1, 500.50m, 1 },
+                    { 2, 2, 340.00m, 2 },
+                    { 3, 3, 700.00m, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Purchased_Ticket",
+                columns: new[] { "CustomerId", "TicketConcertId", "PurchaseDate" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 2, new DateTime(2025, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, 3, new DateTime(2024, 9, 19, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchased_Ticket_CustomerId",
+                table: "Purchased_Ticket",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketConcerts_ConcertId",
+                table: "TicketConcerts",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketConcerts_TicketId",
+                table: "TicketConcerts",
+                column: "TicketId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchased_Ticket");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "TicketConcerts");
+
+            migrationBuilder.DropTable(
+                name: "Concerts");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}

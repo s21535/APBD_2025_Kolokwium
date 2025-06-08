fd23c975a8bf9cf84c515ffc775ff72f6093d75d
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-06-08 09:59:37 +0200

    controller WIP

---
+++ Shows.API/Shows.API/Models/DTOs/GetPurchasesDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Shows.API.Models.DTOs;
+
+public class GetPurchasesDTO
+{
+    public int TicketConcertId { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime PurchaseDate { get; set; }
+}
